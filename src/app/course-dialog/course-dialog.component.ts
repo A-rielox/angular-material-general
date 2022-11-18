c23d255dbf94c78338a0a6bece179188015bb10e
A-rielox <arielox.ag@gmail.com>
2022-11-18 00:27:51 -0600

    listo

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,24 +1,66 @@
-import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import * as moment from 'moment';
+import { Component, Inject, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+   MAT_DIALOG_DATA,
+   MatDialog,
+   MatDialogConfig,
+   MatDialogRef,
+} from "@angular/material/dialog";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import * as moment from "moment";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+   selector: "course-dialog",
+   templateUrl: "./course-dialog.component.html",
+   styleUrls: ["./course-dialog.component.css"],
 })
 export class CourseDialogComponent implements OnInit {
+   description: string;
 
-    constructor(private fb: FormBuilder) {
+   form = this.fb.group({
+      description: [this.course.description, Validators.required],
+      category: [this.course.category, Validators.required],
+      releasedAt: [new Date(), Validators.required],
+      longDescription: [this.course.longDescription, Validators.required],
+   });
 
+   constructor(
+      private fb: FormBuilder,
+      @Inject(MAT_DIALOG_DATA) private course: Course,
+      private dialogRef: MatDialogRef<CourseDialogComponent>
+   ) {
+      this.description = course.description;
+   }
 
-    }
+   ngOnInit() {}
 
-    ngOnInit() {
+   close() {
+      this.dialogRef.close();
+   }
 
-    }
+   save() {
+      // p' pasar los valores editados a quien llamo el save
+      this.dialogRef.close(this.form.value);
+   }
 
+   // HACER la fcn p' abrir el modal aca, p' poder reocupar la fcn donde sea q se requiera
 }
 
+export function openEditCourseDialog(dialog: MatDialog, course: Course) {
+   const config = new MatDialogConfig();
+
+   config.disableClose = false; // cierro con esc y picando afuera
+   config.autoFocus = true;
+   config.panelClass = "modal-panel";
+   // config.backdropClass = "backdrop-modal-panel";
+   config.enterAnimationDuration = "2000ms";
+
+   // p' pasar la data al modal
+   config.data = {
+      ...course,
+   };
+
+   const dialogRef = dialog.open(CourseDialogComponent, config);
+
+   return dialogRef.afterClosed();
+}
--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
 import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { openEditCourseDialog } from "../course-dialog/course-dialog.component";
+import { pipe } from "rxjs";
+import { filter } from "rxjs/operators";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 
 @Component({
    selector: "courses-card-list",
@@ -10,12 +14,50 @@ import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 export class CoursesCardListComponent implements OnInit {
    @Input()
    courses: Course[];
+   cols = 3;
+   rowHeight = "500px";
 
-   constructor() {}
+   handsetPortrait = false; // 🌟 dinamic class
+
+   constructor(
+      private dialog: MatDialog,
+      private responsive: BreakpointObserver
+   ) {}
 
    ngOnInit() {
-      console.log(this.courses);
+      this.responsive
+         .observe([
+            Breakpoints.Tablet,
+            Breakpoints.TabletLandscape,
+            Breakpoints.HandsetPortrait,
+            Breakpoints.HandsetLandscape,
+         ])
+         .subscribe((retult) => {
+            this.cols = 3;
+            this.rowHeight = "500px";
+            this.handsetPortrait = false; // 🌟
+
+            const breakpoints = retult.breakpoints;
+
+            if (breakpoints[Breakpoints.TabletPortrait]) {
+               this.cols = 1;
+            } else if (breakpoints[Breakpoints.HandsetPortrait]) {
+               this.cols = 1;
+               this.rowHeight = "400px";
+               this.handsetPortrait = true; // 🌟
+            } else if (breakpoints[Breakpoints.HandsetLandscape]) {
+               this.cols = 1;
+            } else if (breakpoints[Breakpoints.TabletLandscape]) {
+               this.cols = 2;
+            }
+         });
    }
 
-   editCourse(course: Course) {}
+   editCourse(course: Course) {
+      // subscribe p' agarrar los valores editados, emite valores despues de cerrado ( afterClose )
+      // si simplemente cierra el modal si guardar => no se pasan valores y no llego al subscribe, xlo q simplemente se cierra
+      openEditCourseDialog(this.dialog, course)
+         .pipe(filter((val) => !!val))
+         .subscribe((val) => console.log("new value: ", val));
+   }
 }
